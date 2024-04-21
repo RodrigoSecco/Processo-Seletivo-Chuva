b8c282fa21224533ac6cdb25c478feffbb9e531b
RodrigoSecco <rodrigosdn94@gmail.com>
2024-04-21 01:56:32 -0300

    [Processo-Seletivo-Chuva] creation of the createTopic and send functions.

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +7,4 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 })
 export class AppComponent {
   title = 'DevChuva';
-  @ViewChild('resumo') resumoElement: ElementRef;
-
-  toggleResumo() {
-    const resumo = this.resumoElement.nativeElement;
-    resumo.classList.toggle('expandido');
-  }
 }
-
